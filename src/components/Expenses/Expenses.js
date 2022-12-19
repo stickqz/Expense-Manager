26116752941229f941f8e8095cef92c53b0a43df
stickqz <rkmehta246@gmail.com>
2022-12-19 12:14:15 +0530

    add filter and addition componets

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,43 @@
+import { useState } from "react";
+
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2020");
+
+  const filter = (year) => {
+    setSelectedYear(year);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={selectedYear} onChangefilter={filter} />
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+        <ExpenseItem
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        />
+      </Card>
+    </div>
   );
 };
 
